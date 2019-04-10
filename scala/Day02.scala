9090bf5c3688eb7aef11ab84b78cb908cf48aabc
Zera Alexander <zera@excitingfuture.org>
2019-04-09 19:26:21 -0700

    Day 2 in Scala

---
+++ scala/Day02.scala
@@ -0,0 +1,78 @@
+import scala.collection._
+import Keys._
+
+case class Key(name: String, adjacentKeys: mutable.Map[KeypadDirection, Key]) {
+  override def toString: String = this.name
+}
+case class Keypad(keys: List[Key])
+
+object Day02 extends App {
+  val instructions: List[String] = Utils.read(args(0))
+
+  for (part <- 1 to 2) {
+    val keypad: Keypad = initializeKeypad(part)
+    var key: Key = keypad.keys(FIVE)
+
+    instructions.foreach { line =>
+      for (direction <- line)
+        key = move(key, direction)
+
+      print(key)
+    }
+    println()
+  }
+
+  def move(key: Key, dir: KeypadDirection): Key = key.adjacentKeys.getOrElse(dir, key)
+
+  def initializeKeypad(part: Int): Keypad = part match {
+    case 1 =>
+      val one   = Key("1", mutable.Map[KeypadDirection, Key]())
+      val two   = Key("2", mutable.Map[KeypadDirection, Key]())
+      val three = Key("3", mutable.Map[KeypadDirection, Key]())
+      val four  = Key("4", mutable.Map[KeypadDirection, Key]())
+      val five  = Key("5", mutable.Map[KeypadDirection, Key]())
+      val six   = Key("6", mutable.Map[KeypadDirection, Key]())
+      val seven = Key("7", mutable.Map[KeypadDirection, Key]())
+      val eight = Key("8", mutable.Map[KeypadDirection, Key]())
+      val nine  = Key("9", mutable.Map[KeypadDirection, Key]())
+      one.adjacentKeys    += ('D' -> four, 'R' -> two)
+      two.adjacentKeys    += ('D' -> five, 'L' -> one, 'R' -> three)
+      three.adjacentKeys  += ('L' -> two, 'D' -> six)
+      four.adjacentKeys   += ('U' -> one, 'R' -> five, 'D' -> seven)
+      five.adjacentKeys   += ('U' -> two, 'L' -> four, 'R' -> six, 'D' -> eight)
+      six.adjacentKeys    += ('U' -> three, 'L' -> five, 'D' -> nine)
+      seven.adjacentKeys  += ('U' -> four, 'R' -> eight)
+      eight.adjacentKeys  += ('U' -> five, 'L' -> seven, 'R' -> nine)
+      nine.adjacentKeys   += ('U' -> six, 'L' -> eight)
+      Keypad(List(one, two, three, four, five, six, seven, eight, nine))
+
+    case 2 =>
+      val one   = Key("1", mutable.Map[KeypadDirection, Key]())
+      val two   = Key("2", mutable.Map[KeypadDirection, Key]())
+      val three = Key("3", mutable.Map[KeypadDirection, Key]())
+      val four  = Key("4", mutable.Map[KeypadDirection, Key]())
+      val five  = Key("5", mutable.Map[KeypadDirection, Key]())
+      val six   = Key("6", mutable.Map[KeypadDirection, Key]())
+      val seven = Key("7", mutable.Map[KeypadDirection, Key]())
+      val eight = Key("8", mutable.Map[KeypadDirection, Key]())
+      val nine  = Key("9", mutable.Map[KeypadDirection, Key]())
+      val a     = Key("A", mutable.Map[KeypadDirection, Key]())
+      val b     = Key("B", mutable.Map[KeypadDirection, Key]())
+      val c     = Key("C", mutable.Map[KeypadDirection, Key]())
+      val d     = Key("D", mutable.Map[KeypadDirection, Key]())
+      one.adjacentKeys    += ('D' -> three)
+      two.adjacentKeys    += ('R' -> three, 'D' -> six)
+      three.adjacentKeys  += ('L' -> two, 'U' -> one, 'R' -> four, 'D' -> seven)
+      four.adjacentKeys   += ('L' -> three, 'D' -> eight)
+      five.adjacentKeys   += ('R' -> six)
+      six.adjacentKeys    += ('L' -> five, 'U' -> two, 'R' -> seven, 'D' -> a)
+      seven.adjacentKeys  += ('L' -> six, 'U' -> three, 'R' -> eight, 'D' -> b)
+      eight.adjacentKeys  += ('L' -> seven, 'U' -> four, 'R' -> nine, 'D' -> c)
+      nine.adjacentKeys   += ('L' -> eight)
+      a.adjacentKeys      += ('U' -> six, 'R' -> b)
+      b.adjacentKeys      += ('L' -> a, 'U' -> seven, 'R' -> c, 'D' -> d)
+      c.adjacentKeys      += ('U' -> eight, 'L' -> b)
+      d.adjacentKeys      += ('U' -> b)
+      Keypad(List(one, two, three, four, five, six, seven, eight, nine, a, b, c, d))
+  }
+}
--- scala/Utils.scala
+++ scala/Utils.scala
@@ -9,13 +9,36 @@ object Utils {
   }
 }
 
-object Direction extends Enumeration {
-  type Direction = Value
+object MapDirection extends Enumeration {
+  type MapDirection = Value
   val NORTH, EAST, SOUTH, WEST = Value
 }
 
-object Orientation extends Enumeration {
-  type Orientation = Value
-  val RIGHT = Value("R")
-  val LEFT = Value("L")
+object TurnDirection extends Enumeration {
+  type TurnDirection = Value
+  val LEFT = Value('L')
+  val RIGHT = Value('R')
 }
+
+object Keys {
+  val ONE = 0
+  val TWO = 1
+  val THREE = 2
+  val FOUR = 3
+  val FIVE = 4
+  val SIX = 5
+  val SEVEN = 6
+  val EIGHT = 7
+  val NINE = 8
+  type KeypadDirection = Char
+  // 'U', 'D', 'L', 'R'
+
+}
+
+//object KeypadDirection extends Enumeration {
+//  type KeypadDirection = Value
+//  val UP = Value('U')
+//  val DOWN = Value('D')
+//  val LEFT = Value('L')
+//  val RIGHT = Value('R')
+//}
